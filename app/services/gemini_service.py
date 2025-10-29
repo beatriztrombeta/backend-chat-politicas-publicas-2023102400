69eb6f463d1a4ec5882c7d38d04bf882c68392ef
Beatriz Trombeta <beatriztrombeta@gmail.com>
2025-10-29 11:24:40 -0300

    updated gemini service

--- app/services/gemini_service.py
+++ app/services/gemini_service.py
@@ -1,18 +1,40 @@
 import httpx
 from app.config import settings
+from app.data.questions_list import QUESTIONS_LIST
 
 def ask_gemini(question: str):
-    # Exemplo de request fictício
-    url = "https://api.gemini.com/v1/chat"
-    headers = {"Authorization": f"Bearer {settings.GEMINI_API_KEY}"}
-    payload = {"question": question}
+    url = "https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent"
+    headers = {"Content-Type": "application/json"}
+    params = {"key": settings.GEMINI_API_KEY}
+    prompt = f"""
+        Analise a pergunta do usuário e diga qual das 50 perguntas predefinidas ela mais se assemelha.
+        Responda APENAS com o número correspondente (1 a 50). Não explique.
+
+        {QUESTIONS_LIST}
+
+        Pergunta do usuário: "{question}"
+        """
+    payload = {"contents": [{"parts": [{"text": prompt}]}]}
 
     try:
-        response = httpx.post(url, json=payload, headers=headers, timeout=10)
+        response = httpx.post(url, params=params, json=payload, headers=headers, timeout=60)
+        response.raise_for_status()
         data = response.json()
-        return {
-            "answer": data.get("answer"),
-            "question_id": data.get("question_id")
-        }
-    except Exception:
+        print("Resposta da Gemini API:", data)
+
+        answer = (
+            data.get("candidates", [{}])[0]
+            .get("content", {})
+            .get("parts", [{}])[0]
+            .get("text", "")
+            .strip()
+        )
+
+        return {"answer": answer}
+
+    except httpx.HTTPStatusError as e:
+        print("Erro HTTP:", e.response.status_code, e.response.text)
+        return None
+    except Exception as e:
+        print("Erro geral:", e)
         return None
