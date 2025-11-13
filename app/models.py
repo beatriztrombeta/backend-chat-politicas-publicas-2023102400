54e81ca854b9c83e494f3881c75bc2a7ee3320ad
beatriztrombeta <beatriztrombeta@gmail.com>
2025-11-13 14:51:22 -0300

    Removed Reports table from models

--- app/models.py
+++ app/models.py
@@ -5,9 +5,3 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, nullable=False)
-
-class Report(Base):
-    __tablename__ = "reports"
-    id = Column(Integer, primary_key=True, index=True)
-    questionID = Column(Integer, nullable=False)
-    link = Column(String, nullable=False)
